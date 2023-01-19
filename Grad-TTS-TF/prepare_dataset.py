74b90934de13beb0e05f488688e3025aad1c1d7f
Diego <dmmagdal@ucsc.edu>
2023-01-19 12:43:02 -0800

    Added prepare_dataset.py to preview loading the LJSpeech dataset for GradTTS. Also added train.py to get a start on tracing the flow of data through the GradTTS model.

---
+++ Grad-TTS-TF/prepare_dataset.py
@@ -0,0 +1,100 @@
+# prepare_dataset.py
+
+
+import time
+from pathlib import Path
+import tensorflow as tf
+import tqdm
+from data import Data
+import params
+
+
+def main():
+	text_cleaners = ['english_cleaners_v2']
+	dataset_path = './ljspeech_train'
+	filelist = './filelists/ljs_audio_text_train_v3.txt'
+	# filelist = './filelists/ljs_audio_text_val.txt'
+
+	extract_mels = True
+	max_wav_value = 32768.0
+
+	# mel extraction
+	# n_speakers = 1
+	# max_wav_value = 32768.0
+	# sampling_rate = 22050
+	# filter_length = 1024
+	# hop_length = 256
+	# win_length = 1024
+	# mel_fmin = 0.0
+	# mel_fmax = 8000.0
+	# n_mel_channels = 80
+	# f0_method = 'pyin'
+	# batch_size = 1
+
+	# dataset = TTSDataset(
+	dataset = Data(
+		dataset_path, # dataset_path
+		filelist, # filelist_path
+		params.cmudict_path, # cmudict_path
+		# n_mel_channels=n_mel_channels,
+		n_mel_channels=params.n_feats,
+		n_speakers=params.n_spks,
+		load_mel_from_disk=False,
+		# sampling_rate=sampling_rate,
+		sampling_rate=params.sample_rate,
+		# filter_length=filter_length,
+		filter_length=params.n_fft,
+		hop_length=params.hop_length,
+		win_length=params.win_length,
+		# mel_fmin=mel_fmin,
+		# mel_fmax=mel_fmax
+		mel_fmin=params.f_min,
+		mel_fmax=params.f_max
+	)
+
+	# Additional code.
+	print(dataset.__getitem__(0))
+	# for i in dataset.__getitem__(0):
+	# 	print(i)
+	# for i in range(dataset.__len__()):
+	# 	dataset.__getitem__(i)
+
+	# Outputs are the following:
+	# -> padded encoded texts (dtype=tf.int64, 
+	#	shape=(max_input_length))
+	# -> input lengths (dtype=tf.int64, shape=())
+	# -> padded mel spectrograms (dtype=tf.float32,
+	#	shape=(max_target_length, n_mel_channels))
+	# -> output lengths (dtype=tf.int64, shape=())
+	# -> text length (dtype=tf.int64, shape=())
+	# -> padded pitch (dtype=tf.float32, 
+	#	shape=(n_formants, max_target_lengths + 4))
+	# -> padded energy (dtype=tf.float32,
+	#	shape=(max_target_length,))
+	# -> speaker id (dtype=tf.int64, shape=())
+	# -> padded attention priors (dtype=tf.float32,
+	#	shape=(max_target_length, max_input_length))
+	# -> audiopath (dtype=tf.string, shape=())
+
+	data = tf.data.Dataset.from_generator(
+		dataset.generator,
+		args=(),
+		output_signature=(
+			tf.TensorSpec(shape=(None,), dtype=tf.int64),			# text_encoded
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# input_lengths
+			tf.TensorSpec(
+				# shape=(None, n_mel_channels), dtype=tf.float32
+				shape=(None, params.n_feats), dtype=tf.float32
+			),														# mel
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# output_lengths
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# speaker id
+		)
+	)
+	print(list(data.as_numpy_iterator())[0])
+
+	# Exit the program.
+	exit(0)
+
+
+if __name__ == '__main__':
+	main()
---
+++ Grad-TTS-TF/train.py
@@ -0,0 +1,149 @@
+# train.py
+#
+# Tensorflow 2.7.0
+# Windows/MacOS/Linux
+# Python 3.7
+
+
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+from tqdm import tqdm
+import params
+from data import Data
+from model import GradTTS
+from utils import plot_tensor, save_plot
+from text.symbols import symbols
+
+
+def main():
+	# Set seed.
+	tf.random.set_seed(params.seed)
+	np.random.seed(params.seed)
+
+	# Initialize logger.
+
+	# Initialize data loaders/dataset.
+	dataset_path = './ljspeech_train'
+	train_data = Data(
+		dataset_path, # dataset_path
+		params.train_filelist_path, # filelist_path
+		params.cmudict_path, # cmudict_path
+		n_mel_channels=params.n_feats,
+		n_speakers=params.n_spks,
+		load_mel_from_disk=True,#False,
+		sampling_rate=params.sample_rate,
+		filter_length=params.n_fft,
+		hop_length=params.hop_length,
+		win_length=params.win_length,
+		mel_fmin=params.f_min,
+		mel_fmax=params.f_max
+	)
+	train_dataset = tf.data.Dataset.from_generator(
+		train_data.generator,
+		args=(),
+		output_signature=(
+			tf.TensorSpec(shape=(None,), dtype=tf.int64),			# text_encoded
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# input_lengths
+			tf.TensorSpec(
+				# shape=(None, n_mel_channels), dtype=tf.float32
+				shape=(None, params.n_feats), dtype=tf.float32
+			),														# mel
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# output_lengths
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# speaker id
+		)
+	)
+	train_dataset = train_dataset.batch(16, drop_remainder=True)
+	valid_data = Data(
+		dataset_path, # dataset_path
+		params.valid_filelist_path, # filelist_path
+		params.cmudict_path, # cmudict_path
+		n_mel_channels=params.n_feats,
+		n_speakers=params.n_spks,
+		load_mel_from_disk=True,#False,
+		sampling_rate=params.sample_rate,
+		filter_length=params.n_fft,
+		hop_length=params.hop_length,
+		win_length=params.win_length,
+		mel_fmin=params.f_min,
+		mel_fmax=params.f_max
+	)
+	valid_dataset = tf.data.Dataset.from_generator(
+		valid_data.generator,
+		args=(),
+		output_signature=(
+			tf.TensorSpec(shape=(None,), dtype=tf.int64),			# text_encoded
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# input_lengths
+			tf.TensorSpec(
+				# shape=(None, n_mel_channels), dtype=tf.float32
+				shape=(None, params.n_feats), dtype=tf.float32
+			),														# mel
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# output_lengths
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# speaker id
+		)
+	)
+	test_data = Data(
+		dataset_path, # dataset_path
+		params.test_filelist_path, # filelist_path
+		params.cmudict_path, # cmudict_path
+		n_mel_channels=params.n_feats,
+		n_speakers=params.n_spks,
+		load_mel_from_disk=True,#False,
+		sampling_rate=params.sample_rate,
+		filter_length=params.n_fft,
+		hop_length=params.hop_length,
+		win_length=params.win_length,
+		mel_fmin=params.f_min,
+		mel_fmax=params.f_max
+	)
+	test_dataset = tf.data.Dataset.from_generator(
+		test_data.generator,
+		args=(),
+		output_signature=(
+			tf.TensorSpec(shape=(None,), dtype=tf.int64),			# text_encoded
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# input_lengths
+			tf.TensorSpec(
+				# shape=(None, n_mel_channels), dtype=tf.float32
+				shape=(None, params.n_feats), dtype=tf.float32
+			),														# mel
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# output_lengths
+			tf.TensorSpec(shape=(), dtype=tf.int64),				# speaker id
+		)
+	)
+
+	# Initialize model.
+	model = GradTTS(
+		params.n_symbols, 1, None, params.n_enc_channels, 
+		params.filter_channels, params.filter_channels_dp, 
+		params.n_heads, params.n_enc_layers, params.enc_kernel,
+		params.enc_dropout, params.window_size, params.n_feats, 
+		params.dec_dim, params.beta_min, params.beta_max, 
+		params.pe_scale, params.out_size
+	)
+
+	# Get number of encoder parameters.
+
+	# Get number of decoder parameters.
+
+	# Get total number of parameters.
+
+	# Initialize optimizer.
+	optimizer = Adam(learning_rate=params.learning_rate)
+
+	# Start training.
+	model.compile(
+		optimizer=optimizer, #loss=[loss, attention_kl_loss],
+		run_eagerly=True
+	)
+
+	model.fit(train_dataset, epochs=1, batch_size=16)
+
+
+	# Exit the program.
+	exit(0)
+
+
+if __name__ == '__main__':
+	main()
